7d81dc2ff5adfa5140f2f5bce1488e5cb40e946a
Alex Mekovsky <118875186+Alexander-Mekovsky@users.noreply.github.com>
2022-11-28 09:34:47 -0500

    Add files via upload

---
+++ Lab02/array.cpp
@@ -0,0 +1,101 @@
+//Name: Alexander Mekovsky
+//Date: September 6, 2022
+//Class: COSC 220
+//Assignment: Lab 02 Part 1
+//Program Objective: To make an array with parameters the user must decide, including size of array and range of numbers that the array can generate, the program shall then 
+//count each occurring value and print how many values the array has at the end of the program.
+
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int* generateNumbers(int, int);
+void displayArray(int[], int);
+void arrayCount(int[], int);
+
+int main()
+{
+    int range, size;
+    char show;
+    cout << "Input size of array: ";
+    cin >> size;
+    if(size <= 0)
+    {
+        do
+        {
+            cout << "Invalid size detected. Please enter another value: ";
+            cin >> size;
+        } while (size <= 0);
+    }
+    cout << "Input n > 0 for upper bound on range of random numbers, [1, n]: ";
+    cin >> range;
+    if(range <= 0)
+    {
+        do
+        {
+            cout << "Invalid range detected. Please enter another value: ";
+            cin >> range;
+        } while (range <= 0);
+    }
+    int *arrayGen = generateNumbers(range, size);
+    cout << "Do you want a printout of the array> (Y/N) :";
+    cin >> show;
+    show = toupper(show);
+    if(show != 'Y' && show != 'N')
+    {
+        do
+        {
+            cout << "Invalid choice. Please try again: (Y/N) :";
+            cin >> show;
+        } while (show != 'Y' && show != 'N');
+    }
+    if(show == 'Y')
+    {
+        displayArray(arrayGen, size);
+    }
+    for(int i = 0; i < range; i++)
+    {        
+        arrayCount(arrayGen, size);
+    }
+    return 0;
+}
+
+void arrayCount(int arr[], int size)
+{
+    int count = 0;
+    static int interval = 1;
+    for(int i = 0; i < size; i++)
+    {
+        if(arr[i] == interval)
+        {
+            count++;
+        }
+    }
+    cout << interval << ": " << count << endl;
+    interval++;
+}
+
+int* generateNumbers(int range, int size)
+{
+    int *arr = nullptr;
+    arr = new int[size];
+    srand(time(0));
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % range + 1;
+    }
+    return arr;
+}
+
+void displayArray(int arr[], int size)
+{
+    cout << "Array" << endl;
+    for(int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
---
+++ Lab02/dice.cpp
@@ -0,0 +1,105 @@
+//Name: Alexander Mekovsky
+//Date: September 6, 2022
+//Class: COSC 220
+//Assignment: Lab 02 Part 2
+//Program Objective: To make an array bound by the limits of a number of dice. The user must decide how many rolls they want and with how many dice each roll shall have. At the end
+//of the program each value rolled up to that point will be counted and added at the end similar to the previous program.
+
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int* generateNumbers(int, int);
+void displayArray(int[], int);
+void arrayCount(int[], int);
+
+int main()
+{
+    int range, size;
+    char show;
+    cout << "Input number of rolls: ";
+    cin >> size;
+    if(size <= 0)
+    {
+        do
+        {
+            cout << "Invalid roll detected. Please enter another value: ";
+            cin >> size;
+        } while (size <= 0);
+    }
+    cout << "Input number of dice: ";
+    cin >> range;
+    if(range <= 0)
+    {
+        do
+        {
+            cout << "Invalid dice detected. Please enter another value: ";
+            cin >> range;
+        } while (range <= 0);
+    }
+    int *arrayGen = generateNumbers(range, size);
+    cout << "Do you want a printout of the array> (Y/N) :";
+    cin >> show;
+    show = toupper(show);
+    if(show != 'Y' && show != 'N')
+    {
+        do
+        {
+            cout << "Invalid choice. Please try again: (Y/N) :";
+            cin >> show;
+        } while (show != 'Y' && show != 'N');
+    }
+    if(show == 'Y')
+    {
+        displayArray(arrayGen, size);
+    }
+    cout << "Counts" << endl;
+    for(int i = 0; i < (range * 6); i++)
+    {        
+        arrayCount(arrayGen, size);
+    }
+    return 0;
+}
+
+void arrayCount(int arr[], int size)
+{
+    int count = 0;
+    static int interval = 1;
+    for(int i = 0; i < size; i++)
+    {
+        if(arr[i] == interval)
+        {
+            count++;
+        }
+    }
+    cout << interval << ": " << count << endl;
+    interval++;
+}
+
+int* generateNumbers(int range, int size)
+{
+    int *arr = nullptr;
+    arr = new int[size];
+    srand(time(0));
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < range; j++)
+        {
+            arr[i] += (rand() % 6) + 1;
+        }
+    }
+    return arr;
+}
+
+void displayArray(int arr[], int size)
+{
+    cout << "Rolls" << endl;
+    for(int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
